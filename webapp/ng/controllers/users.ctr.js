3e5280af5e1adbf33b68351afaaa90e505734a77
Christian Castro <castroMBair@MacBook-Air.local>
2016-08-10 15:02:08 -0500

    Add Score. Up Next: Stop Overwriting Behavior

--- webapp/ng/controllers/users.ctr.js
+++ webapp/ng/controllers/users.ctr.js
@@ -6,8 +6,8 @@
   angular
     .module('rpsApp')
     .controller('usersCtr',
-            ['$scope','$state','$http','$mdToast','$mdDialog','$firebaseObject','authService',
-      function($scope, $state, $http, $mdToast, $mdDialog, $firebaseObject, authService){
+            ['$scope','$state','$http','$mdToast','$mdDialog','$firebaseObject','authService','addUserService',
+      function($scope, $state, $http, $mdToast, $mdDialog, $firebaseObject, authService, addUserService){
 
       let t = this;
       let s = $scope;
@@ -17,6 +17,8 @@
 
 
       //vars
+      s.users = {};
+      // $firebaseObject(firebase.database().ref().child("users")).$bindTo(s,"users");
 
 
       let ref = firebase.database().ref().child("data");
@@ -25,30 +27,7 @@
 
       syncObject.$bindTo(s, "data");
 
-
-      //BEGIN AUTH
-      s.createUser = function() {
-        s.message = null;
-        s.error = null;
-        authService.$createUserWithEmailAndPassword(s.email,s.password)
-          .then(function(firebaseUser) {
-            s.message = "User created with uid: " + firebaseUser.uid;
-          }).catch(function(error) {
-            s.error = error;
-          });
-      };
-
-      let usersObj = firebase.database().ref().child("users");
-      ref.onAuth(function(authData) {
-        if (authData && isNewUser) {
-          // save the user's profile into the database so we can list users,
-          // use them in Security and Firebase Rules, and show profiles
-          ref.child("users").child(authData.uid).set({
-            provider: authData.provider,
-            name: getName(authData)
-          });
-        }
-      });
+      //       name: getName(authData)
 
       //DELETE USER
       s.deleteUser = function() {
@@ -56,18 +35,136 @@
         s.error = null;
         authService.$deleteUser().then(function() {
           s.message = "Sorry to see you go. Your account has been removed.";
-        }).catch(function(error) {
-          s.error = error;
-        });
+          })
+          .catch(function(error) {
+            s.error = error;
+          });
       };
 
-      //RETRIEVE AUTHENTICATED STATE
+      //EMAIL SIGNUPS
+      s.createUser = function() {
+        s.message = null;
+        s.error = null;
+        authService.$createUserWithEmailAndPassword(s.email,s.password)
+          .then(function(firebaseUser) {
+
+            s.firebaseUser = firebaseUser;
+
+            Object.defineProperties(s.firebaseUser.providerData[0],{
+              displayName: { writable:true },
+              email: { writable:true },
+              photoURL: { writable:true }
+            });
+
+            s.firebaseUser.providerData[0].displayName;
+            $scope.$watch('firebaseUser.providerData[0].displayName', function(){
+                if (s.firebaseUser){
+                  s.firebaseUser.providerData[0].displayName = s.firebaseUser.providerData[0].email.replace(/@.*/, '');
+                }
+              }
+            );
+            s.firebaseUser.providerData[0].displayName = s.firebaseUser.providerData[0].email.replace(/@.*/, '');
+
+            s.firebaseUser.providerData[0].photoURL = 'assets/images/no-photo.gif';
+
+            s.firebaseUser.providerData[0].score = 0;
+
+            if (s.firebaseUser !== null && s.firebaseUser.providerData[0].providerId === 'password'){
+
+              var ref = firebase.database().ref().child("users");
+              var users = $firebaseObject(ref);
+              var uid = s.firebaseUser.uid;
+
+              users[uid] = s.firebaseUser.providerData[0];
+
+              users.$save()
+              .then(function(ref) {
+                console.log('Email Signup Saved');
+              }, function(error) {
+                console.log("Oops, the following went wrong: ", error);
+              });
+
+            } else {
+              console.log('is logged out');
+            }
+
+
+          })
+          .catch(function(error) {
+            s.error = error;
+          });
+      };
+
+      //SOCIAL SIGNUPS
       s.auth = authService;
-      // any time auth state changes, add user data to scope
-      s.auth.$onAuthStateChanged(function(firebaseUser) {
+      authService.$onAuthStateChanged(function(firebaseUser){
         s.firebaseUser = firebaseUser;
-        console.log(firebaseUser);
+        if (s.firebaseUser){
+
+          if (s.firebaseUser.providerData[0].providerId !== "password"){
+
+            // function addUser(child){
+            //
+            // }
+
+            var ref = firebase.database().ref().child("users");
+            var users = $firebaseObject(ref);
+            var uid = s.firebaseUser.uid;            
+
+            s.firebaseUser.providerData[0].score = 0;
+
+            users[uid] = s.firebaseUser.providerData[0];
+
+            users.$save()
+            .then(function(ref) {
+              console.log('Social Signup Saved');
+            }, function(error) {
+              console.log("Oops, the following went wrong: ", error);
+            });
+
+          }
+        } else {
+          console.log('is logged out');
+        }
+
+
+
       });
+
+      // //RETRIEVE AUTHENTICATED STATE
+      // s.auth = authService;
+      // // any time auth state changes, add user data to scope
+      // s.auth.$onAuthStateChanged(function(firebaseUser) {
+      //   if (firebaseUser){
+      //     s.user = firebaseUser;
+      //     s.uid = s.user.uid;
+      //
+      //   let ref = firebase.database().child("users").child(s.uid);
+      //
+      //   addUserService(ref)
+      //     .then(function(){
+      //       alert('user added to db');
+      //     },function(error){
+      //       console.log('Oops:', error);
+      //     });
+      //
+      //   console.log(s.firebaseUser.uid);
+      //
+      //   }
+      // });//END RETRIEVE AUTHENTICATED STATE
+
+      //EXAMPLE
+      // {
+      //   "profiles": {
+      //      "physicsmarie": {
+      //         name: "Marie Curie",
+      //         dob: "November 7, 1867"
+      //      }
+      //   }
+      // }
+      // var ref = firebase.database().ref();
+      // $scope.profile = $firebaseObject(ref.child('profiles').child('physicsmarie'));
+
 
       function showToast(message){
 
--- webapp/ng/services.js
+++ webapp/ng/services.js
@@ -3,7 +3,9 @@
   'use strict';
 
   angular
+
     .module('rpsApp')
+
     .service('getUsersService',['$scope','$http','$firebaseObject',
                         function($scope, $http, $firebaseObject){
 
@@ -16,6 +18,19 @@
       return $firebaseAuth();
 
     }])
+
+    .service("addUserService", ["$firebaseObject",
+      function($firebaseObject) {
+        return function(uid) {
+          // create a reference to the database node where we will store our data
+          var ref = firebase.database().ref("users").push();
+          var uidRef = ref.child(uid);
+
+          // return it as a synchronized object
+          return $firebaseObject(uidRef);
+        }
+      }
+    ])
 
     .service('newGameService',['$http','$firebaseObject',function($http,$firebaseObject){
 
